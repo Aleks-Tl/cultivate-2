458d7a5731b0ee0e9d42fadb8871b131240b9b4d
Aleksandr Tloka <aleksandr.tloka@i.ua>
2022-06-09 23:21:04 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+import './_vendor';
+import vars from './_vars';
+import './_functions';
+import './_components';
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.querySelector('header').classList.add('backgroundWhite');
+  } else {
+    document.querySelector('header').classList.remove('backgroundWhite');
+  }
+}
+
+// Swiper for home page
+
+const swiper = new Swiper(".members__slider", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 768px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
+});
+
+
+

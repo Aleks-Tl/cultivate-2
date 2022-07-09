5e2cab1f19728438fb2a7d0f5342e4e099e4c9bd
Aleksand Tloka <aleksandr.tloka@i.ua>
2022-07-09 20:41:53 +0300

    fixed filters

--- src/js/main.js
+++ src/js/main.js
@@ -268,34 +268,59 @@ if (document.querySelectorAll('.accordion__list').length > 0) {
 
 // Filter
 
-if (document.querySelectorAll('.filter__select').length > 0) {
-  let select = function () {
-    let selectHeader = document.querySelectorAll('.select__header');
-    let selectItem = document.querySelectorAll('.select__item');
+/*
+Reference: http://jsfiddle.net/BB3JK/47/
+*/
 
-    selectHeader.forEach(item => {
-      item.addEventListener('click', selectToggle);
-    });
+$('select').each(function () {
+  var $this = $(this), numberOfOptions = $(this).children('option').length;
 
-    selectItem.forEach(item => {
-      item.addEventListener('click', selectChoose);
-    });
+  $this.addClass('select-hidden');
+  $this.wrap('<div class="select"></div>');
+  $this.after('<div class="select-styled"></div>');
 
-    function selectToggle() {
-      this.parentElement.classList.toggle('is-active');
-    }
+  var $styledSelect = $this.next('div.select-styled');
+  $styledSelect.text($this.children('option').eq(0).text());
 
-    function selectChoose() {
-      let text = this.innerText,
-        select = this.closest('.filter__select'),
-        currentText = select.querySelector('.select__current');
-      currentText.innerText = text;
-      select.classList.remove('is-active');
-    }
+  var $list = $('<ul />', {
+    'class': 'select-options'
+  }).insertAfter($styledSelect);
+
+  for (var i = 0; i < numberOfOptions; i++) {
+    $('<li />', {
+      text: $this.children('option').eq(i).text(),
+      rel: $this.children('option').eq(i).val()
+    }).appendTo($list);
+    //if ($this.children('option').eq(i).is(':selected')){
+    //  $('li[rel="' + $this.children('option').eq(i).val() + '"]').addClass('is-selected')
+    //}
   }
 
-  select();
-}
+  var $listItems = $list.children('li');
+
+  $styledSelect.click(function (e) {
+    e.stopPropagation();
+    $('div.select-styled.active').not(this).each(function () {
+      $(this).removeClass('active').next('ul.select-options').hide();
+    });
+    $(this).toggleClass('active').next('ul.select-options').toggle();
+  });
+
+  $listItems.click(function (e) {
+    e.stopPropagation();
+    $styledSelect.text($(this).text()).removeClass('active');
+    $this.val($(this).attr('rel'));
+    $list.hide();
+    //console.log($this.val());
+  });
+
+  $(document).click(function () {
+    $styledSelect.removeClass('active');
+    $list.hide();
+  });
+
+});
+
 // Choices select
 
 /* if (document.querySelectorAll('.filter__select').length > 0) {
